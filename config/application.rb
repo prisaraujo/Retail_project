a247ab2a87dbf8d74c0bd130cc4c2a4286a24aba
Priscila <prisaraujo92@gmail.com>
2014-07-02 19:53:54 -0400

    authentication added

--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,4 @@ module RetailDinners
     # config.i18n.default_locale = :de
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :items
-  root 'items#index'
+  root to: 'items#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
