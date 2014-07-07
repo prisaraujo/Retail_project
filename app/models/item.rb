0f6bbed2acc950351520e8c1f173d572d6bf1903
Priscila <prisaraujo92@gmail.com>
2014-07-07 10:25:13 -0400

    database specification changed from sqlite to postgresql

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,3 @@
 class Item < ActiveRecord::Base
-      # scope :title, -> (title) {where title: title}
-      # scope :city, -> (city) {where city: city}
-      # scope :attendence, -> (attendence) {where attendence: attendence}
-      # scope :followup_ownedby, -> (followup_ownedby) {where followup_ownedby: followup_ownedby}
-      # scope :followup_status, -> (followup_status) {where followup_status: followup_status}
-      # scope :comments, -> (comments) {where comments: comments}
-      # scope :name, -> (name) {where name: name}
-      # scope :company, -> (company) {where company: company}
-      # scope :role, -> (role) {where role: role}
-      # scope :emailaddress, -> (emailaddress) {where emailaddress: emailaddress}
-      # scope :address, -> (address) {where address: address}
-      # scope :phone, -> (phone) {where phone: phone}
+
 end
